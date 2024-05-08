a6b21f4de6ed48fb5f03314cf57d62ed7f5ab4e7
Hemant <hemantkumarsonkar1@gmail.com>
2024-05-09 01:00:24 +0530

    useFetch Custom Hook completed

--- components/useFetch.js
+++ components/useFetch.js
@@ -1,3 +1,31 @@
 //write your code here
 
-export default useFetch
+import { useEffect, useState } from "react";
+
+export default function useFetch(url){
+    const [data, setData] = useState();
+    const [loading, setLoading] = useState(false);
+    const [error, setError] = useState(false);
+
+
+    useEffect(() => {
+        async function fetchData(){
+            try{
+                const response = await fetch(url);
+                const data = await response.json();
+
+                setData(data);
+                // console.log(data);
+
+            }catch(error){
+                setError(error);
+            }finally{
+                setLoading(false);
+            }
+        }
+        fetchData();
+    }, [url]);
+
+
+    return {data, loading, error};
+}
